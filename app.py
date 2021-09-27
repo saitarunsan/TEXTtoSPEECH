5f0535fa768f1bf792107762d75e3baf8b27e1a2
sai tarun <saitarunsan@gmail.com>
2021-09-27 18:21:34 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+from playsound import playsound
+
+language = 'en'
+audio = 'voice.mp3'
+
+text = gTTS(text='hello developer...how u doing...its better to work smarter..',lang=language,slow = False)
+text.save(audio)
+playsound(audio)
+print('=====here it now======')
